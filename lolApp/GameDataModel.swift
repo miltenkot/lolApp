ae1f184e8b35fc5b8527db243395bb3499f39d85
miltenkot <miltenkot@gmail.com>
2018-02-20 00:02:32 +0100

    Set App.

---
+++ lolApp/GameDataModel.swift
@@ -0,0 +1,16 @@
+//
+//  userDataModel.swift
+//  lolApp
+//
+//  Created by Bartek Lanczyk on 19.02.2018.
+//  Copyright © 2018 miltenkot. All rights reserved.
+//
+
+import Foundation
+
+class GameDataModel{
+    
+    var gameStartTime : Int = 0
+     
+    
+}
